7fc640e773dbb417ca7c8d5e0329aef2391dd99e
acarrou <dridriswim@gmail.com>
2023-10-20 18:08:42 -0700

    :recycle: refactors code into one config function

--- src/xbee.cpp
+++ src/xbee.cpp
@@ -10,14 +10,10 @@
 
 namespace hal::xbee {
 
-xbee_radio::xbee_radio(hal::serial& p_serial)
-  : m_serial(&p_serial)
+result<xbee_radio> xbee_radio::create(hal::serial& p_serial,
+                                      hal::steady_clock& p_clock)
 {
-}
-
-result<xbee_radio> xbee_radio::create(hal::serial& p_serial)
-{
-  xbee_radio new_xbee(p_serial);
+  xbee_radio new_xbee(p_serial, p_clock);
   return new_xbee;
 }
 
@@ -27,37 +23,59 @@ hal::result<std::span<hal::byte>> xbee_radio::read()
   return data_recieved;
 }
 
-void xbee_radio::write(std::span<const hal::byte> p_data)
+hal::status xbee_radio::write(std::span<const hal::byte> p_data)
 {
   hal::write(*m_serial, p_data);
+  return hal::success();
 }
 
-
-void xbee_radio::write(const char* str)
+hal::status xbee_radio::write(const char* str)
 {
-    auto length = strlen(str);
-    std::span<const hal::byte> span(reinterpret_cast<const hal::byte*>(str), length);
-    write(span);
+  auto length = strlen(str);
+  std::span<const hal::byte> span(reinterpret_cast<const hal::byte*>(str),
+                                  length);
+  write(span);
+  return hal::success();
 }
 
+hal::status xbee_radio::configure_xbee(const char* p_channel,
+                                       const char* p_panid)
+{
 
-void xbee_radio::configure_xbee(const char* p_channel, const char* p_panid) {
-    // Enter command mode
-    write("+++");
-    // Set channel
-    write_command("ATCH", p_channel);
-    // Set PAN ID
-    write_command("ATID", p_panid);
-    // Save configuration
-    write("ATWR\r");
-    // Exit command mode
-    write("ATCN\r");
+  using namespace std::chrono_literals;
+  using namespace hal::literals;
+
+  hal::delay(*m_clock, 500ms);
+  HAL_CHECK(write("+++"));
+  hal::delay(*m_clock, 100ms);
+  auto output = HAL_CHECK(m_serial->read(m_xbee_buffer)).data;
+  hal::delay(*m_clock, 1000ms);
+
+  if (output[0] != 'O' || output[1] != 'K') {
+    return hal::new_error();
+  }
+
+  // // Set channel
+  HAL_CHECK(write_command("ATCH", p_channel));
+  hal::delay(*m_clock, 100ms);
+  // Set PAN ID
+  HAL_CHECK(write_command("ATID", p_panid));
+  hal::delay(*m_clock, 100ms);
+  // Save configuration
+  HAL_CHECK(write("ATWR\r"));
+  hal::delay(*m_clock, 100ms);
+  // Exit command mode
+  HAL_CHECK(write("ATCN\r"));
+  hal::delay(*m_clock, 100ms);
+  return hal::success();
 }
 
-void xbee_radio::write_command(const char* command, const char* value) {
-    write(command);
-    write(value);
-    write("\r");
+hal::status xbee_radio::write_command(const char* command, const char* value)
+{
+  write(command);
+  write(value);
+  write("\r");
+  return hal::success();
 }
 
 }  // namespace hal::xbee
